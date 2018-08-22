32f40527c9824213b08b7f4be3801d1dfb6fd004
jaywcjlove <398188662@qq.com>
2018-08-22 17:06:09 +0800

    Add debug log.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,18 @@
+
+const debug = require('debug')('git:repo');
 const authLoginToAPI = require('../middleware/authLoginToAPI');
 const conf = require('../../conf/conf');
 
 module.exports = (app) => {
   app.git = conf;
   app.use(async (ctx, next) => {
+    debug('%s %s', ctx.method, ctx.url);
     ctx.state.conf = conf;
     await next();
   });
   // Git 相关路由
   app.use(require('./git-client').routes());
+  // 不需要验证的路由
   app.use(authLoginToAPI([
     '/api/user/login', '/api/user/logout', '/api/user/verify'
   ]));
