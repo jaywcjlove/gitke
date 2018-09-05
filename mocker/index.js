c70b1a6a5efcab70654d0840fd24012df7c88b72
jaywcjlove <398188662@qq.com>
2018-09-06 00:35:58 +0800

    Update mocker config.

--- mocker/index.js
+++ mocker/index.js
@@ -1,28 +1,11 @@
 const proxy = {
-  'GET /api/user': { id: 1, username: 'kenny', sex: 6 },
-  'POST /api/login': (req, res) => {
-    const { password, username } = req.body;
-    if (password === 'KKT' && username === 'KKT') {
-      return res.json({
-        code: 0,
-        message: '登录成功！',
-        data: {
-          id: 1,
-          username: 'admin',
-          email: 'admin@admin.com',
-          create_at: '2018-06-13T18:57:41.958Z',
-          update_at: '2018-06-13T18:57:41.958Z',
-        },
-      });
-    }
-    return res.status(401).json({
-      error: 'bad username/password, access denied',
-    });
+  _proxy: {
+    proxy: {
+      '/:owner/:repo/raw/:ref/*': 'http://127.0.0.1:2018'
+    },
+    changeHost: true,
   },
-  'GET /api/user/list': [
-    { id: 1, username: 'kenny', sex: 6 },
-    { id: 2, username: 'kenny', sex: 6 },
-  ],
+  'GET /api/user': { id: 1, username: 'kenny', sex: 6 },
 };
 
 module.exports = proxy;
